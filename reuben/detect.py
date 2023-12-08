794a0f0c69ecf034e65c8ae94b92807231a39b21
Reuben Lewis <reubenlewis28@gmail.com>
2023-12-08 13:53:25 -0500

    Update detect.py

--- reuben/detect.py
+++ reuben/detect.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
-from scipy.interpolate import UnivariateSpline
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression
 
 def update_image(*args):
     # Retrieve the current positions of the trackbars
@@ -12,36 +15,51 @@ def update_image(*args):
     lower_hsv, upper_hsv = calibrate_sidewalk_hsv(hsv_roi, h_range, s_range, v_range)
 
     # Create a mask for the calibrated HSV range and find contours in the ROI
+    mask_roi = cv2.inRange(hsv_roi, lower_hsv, upper_hsv)
+    contours_roi, _ = cv2.findContours(mask_roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Offset the contours by the ROI's starting y-coordinate
+    offset_contours_roi = [contour + (0, roi_y_start) for contour in contours_roi]
+
+    # Create a mask for the entire image
     mask_full = cv2.inRange(hsv, lower_hsv, upper_hsv)
     contours_full, _ = cv2.findContours(mask_full, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Draw the largest contour from the ROI on the original image
     image_copy = original_image.copy()
+    if offset_contours_roi:
+        largest_contour_roi = max(offset_contours_roi, key=cv2.contourArea)
+        cv2.drawContours(image_copy, [largest_contour_roi], -1, (0, 255, 0), 2)  # Green for ROI contour
+
+    # Draw the largest contour from the full image
     if contours_full:
-        # Find the largest contour and its bounding rectangle
         largest_contour_full = max(contours_full, key=cv2.contourArea)
-        cv2.drawContours(image_copy, [largest_contour_full], -1, (0, 255, 255), 3)
+        cv2.drawContours(image_copy, [largest_contour_full], -1, (0, 255, 255), 2)
 
-        # Simplify the contour to a sequence of points
-        curve = np.vstack(largest_contour_full).squeeze()
-        curve = curve[curve[:, 0].argsort()]  # Sort points by x coordinate
+        # Convert the contour to a set of points
+        points = np.vstack(largest_contour_full).squeeze()
+        x = points[:, 0]
+        y = points[:, 1]
 
-        # Fit splines to the sorted points of the contour
-        spline_x = UnivariateSpline(curve[:, 0], curve[:, 1], k=3, s=0.5)
-        spline_y = UnivariateSpline(curve[:, 1], curve[:, 0], k=3, s=0.5)
+        # Fit a polynomial regression model
+        degree = 2  # Degree of the polynomial
+        model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+        model.fit(x.reshape(-1, 1), y)
 
-        # Draw points at regular intervals along the x-axis
-        num_points = 20
-        x_vals = np.linspace(curve[:, 0].min(), curve[:, 0].max(), num_points)
-        y_vals = spline_x(x_vals)
-        points = np.column_stack((x_vals, y_vals)).astype(int)
+        # Generate points along the polynomial curve
+        x_fit = np.linspace(x.min(), x.max(), 500)
+        y_fit = model.predict(x_fit[:, np.newaxis])
 
-        # Draw the points on the image
-        for point in points:
-            cv2.circle(image_copy, tuple(point), 5, (255, 0, 0), -1)
+        # Draw the polynomial curve
+        curve_points = np.array([x_fit, y_fit], dtype=np.int32).T
+        cv2.polylines(image_copy, [curve_points], False, (255, 0, 0), 2)
 
     # Display the updated image
     cv2.imshow('Sidewalk Detection', image_copy)
 
+    # Also display the ROI for reference
+    #roi_image = cv2.cvtColor(mask_roi, cv2.COLOR_GRAY2BGR)  # Convert mask to BGR for display
+    #cv2.imshow('ROI', roi_image)
 
 def calibrate_sidewalk_hsv(hsv_cropped, h_range, s_range, v_range):
     # Assuming the peak of histograms represent the sidewalk
